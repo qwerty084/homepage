b373d34656a6d49a089a9b3e552d8d7698cffe2f
Luca Hendrik Helms <lucahendrik.dev@gmail.com>
2023-01-31 00:04:20 +0100

    add tailwind a start layout

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './assets/style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleMenu = document.getElementById('toggle-menu');
+  const nav = document.getElementById('nav');
+
+  toggleMenu?.addEventListener('click', () => {
+    nav?.classList.toggle('nav-open');
+  });
+});
