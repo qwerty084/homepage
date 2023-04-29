c46a23a8114a000b595349d6090cd20d3216db40
Luca Hendrik Helms <lucahendrik.dev@gmail.com>
2023-04-30 00:17:18 +0200

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgDarkPrimary: '#202124',
+      },
+    },
   },
   plugins: [],
 };
